623b642bf2e35c055afdeec671fe524bdd5ad54b
Rahma2622015 <roramohamed890@gmail.com>
2025-06-18 01:00:42 +0300

    edit in format

--- Ai/englishFunction.py
+++ Ai/englishFunction.py
@@ -96,10 +96,13 @@ def langEnglish(message, storage):
             #print("[DEBUG] Continuing Exam Recommendation Flow")
             s, options = recom_reply.recommender.handle_exam_recommendation(message)
             if is_recommendation_complete(s):
-                if not options:
                     storage.clear_data()
                     storage.set_current_task(None)
                     return s, options, False
+            if not options:
+                storage.clear_data()
+                storage.set_current_task(None)
+                return s, options, False
             return s, options, True
 
         # ---------------------- Course Recommendation System ----------------------
